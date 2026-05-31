e35fb0a7e4f9af43f844428a57153e3a5d2c3e19
Osaf <rajabovosaf6@gmail.com>
2026-05-31 21:51:38 +0300

    Hide footer on messages page

--- client/src/shell/App.jsx
+++ client/src/shell/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function App() {
+  const location = useLocation();
+
+  const isMessagesPage = location.pathname === "/messages";
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
@@ -12,7 +16,7 @@ export default function App() {
         <Outlet />
       </main>
 
-      <Footer />
+      {!isMessagesPage && <Footer />}
     </div>
   );
 }
